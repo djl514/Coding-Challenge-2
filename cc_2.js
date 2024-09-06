ee353bac60311a3ff6dc9c65a1909219b3a1cbb5
David Layne <davidlayne@usf.edu>
2024-09-06 09:25:26 -0400

    Create and test calculateTip function

--- cc_2.js
+++ cc_2.js
@@ -1,11 +1,21 @@
 //1. Calculate the Tip
 //15% if 50 <= x <= 300, else 20%
 
-let bill = 235;
-let tip = bill >=50 && bill <= 300 ? bill*.15: bill*.2;
-let fullBill = bill+tip;
+let check = 275;
+let tip = check >=50 && check <= 300 ? check*.15: check*.2;
+let fullBill = check+tip;
 
 //2. Output Details
-console.log(`The bill was $${bill}, the tip was $${tip}, 
+console.log(`The bill was $${check}, the tip was $${tip}, 
     and the total value is $${fullBill}`);
 
+//3. Create a Function calculateTip
+function calculateTip(bill){
+    if (bill >= 50 && bill <=300){
+        return bill + bill*.15;
+    } else{
+        return bill + bill*.2;
+    }
+}
+
+console.log(`$${calculateTip(100)}`)
